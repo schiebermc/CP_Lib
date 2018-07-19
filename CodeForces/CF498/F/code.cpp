8b8d450ce50e5245305028f906702ede9933ab56
schiebermc <matthewschieber@gmail.com>
2018-07-19 16:28:23 -0400

    solves CF498E

--- CodeForces/CF498/F/code.cpp
+++ CodeForces/CF498/F/code.cpp
@@ -12,52 +12,54 @@ using namespace std;
 
 typedef long long int ll;
 
-// O(>2^n) solution
-ll solution_recursive(ll i, ll j, vector<ll> path, ll n, ll m, ll k, vector<vector<ll>> a);
-ll check_path(vector<ll> path, ll k);
+// total recursion
+// O(>2^n) solution ---------------------------
+//ll solution_recursive(ll i, ll j, vector<ll> path, ll n, ll m, ll k, vector<vector<ll>> a);
+//ll check_path(vector<ll> path, ll k);
+//
+//void solution1(ll n, ll m, ll k, vector<vector<ll>> a){
+//    
+//    ll count = solution_recursive(0, 0, {}, n, m, k, a);
+//    printf("%lld\n", count);
+//}
+//
+//ll solution_recursive(ll i, ll j, vector<ll> path, ll n, ll m, ll k, vector<vector<ll>> a) {
+//
+//    ll count = 0;
+//    if(i == n-1 and j == m-1) {
+//        path.push_back(a[i][j]);
+//        return check_path(path, k);
+//    } 
+//
+//    path.push_back(a[i][j]);
+//    if (i != n-1) {
+//        count += solution_recursive(i+1, j, path, n, m, k, a);
+//    } 
+//
+//    if (j != m-1) {
+//        count += solution_recursive(i, j+1, path, n, m, k, a);
+//    }
+//
+//    return count;
+//
+//}
+//
+//ll check_path(vector<ll> path, ll k) {
+//    //for(auto x : path) {
+//    //    printf("%lld, ", x);
+//    //}
+//    //printf("\n");
+//    ll ans = path[0];
+//    for(ll i=1; i<path.size(); i++) 
+//        ans ^= path[i];
+//    if(ans == k) 
+//        return 1;
+//    else
+//        return 0;
+//}
 
-void solution1(ll n, ll m, ll k, vector<vector<ll>> a){
-    
-    ll count = solution_recursive(0, 0, {}, n, m, k, a);
-    printf("%lld\n", count);
-}
-
-ll solution_recursive(ll i, ll j, vector<ll> path, ll n, ll m, ll k, vector<vector<ll>> a) {
-
-    ll count = 0;
-    if(i == n-1 and j == m-1) {
-        path.push_back(a[i][j]);
-        return check_path(path, k);
-    } 
-
-    path.push_back(a[i][j]);
-    if (i != n-1) {
-        count += solution_recursive(i+1, j, path, n, m, k, a);
-    } 
-
-    if (j != m-1) {
-        count += solution_recursive(i, j+1, path, n, m, k, a);
-    }
-
-    return count;
-
-}
-
-ll check_path(vector<ll> path, ll k) {
-    //for(auto x : path) {
-    //    printf("%lld, ", x);
-    //}
-    //printf("\n");
-    ll ans = path[0];
-    for(ll i=1; i<path.size(); i++) 
-        ans ^= path[i];
-    if(ans == k) 
-        return 1;
-    else
-        return 0;
-}
-
-// O(2^n) solution
+// meet in the middle
+// O(2^n) solution ---------------------------
 void gather_top(ll val, ll i, ll j, ll& n, ll& m, vector<vector<ll>>& a, map<pair<ll, ll>, map<ll, ll>>& top) {
 
     if(i == 0 and j == 0) {
@@ -96,10 +98,10 @@ ll count_paths(ll val, ll i, ll j, ll& n, ll& m, vector<vector<ll>>& a, map<pair
             count += (top[make_pair(i, j-1)].count(val) != 0 ? top[make_pair(i, j-1)][val] : 0);
         }
     } else {
-        if (i != 1) {
+        if (i != 0) {
             count += count_paths(val, i-1, j, n, m, a, top);
         } 
-        if (j != 1) {
+        if (j != 0) {
             count += count_paths(val, i, j-1, n, m, a, top);
         }
     }
@@ -119,15 +121,6 @@ void solution2(ll n, ll m, ll k, vector<vector<ll>> a){
     
     map<pair<ll, ll>, map<ll, ll>> top;
     gather_top(0, 0, 0, n, m, a, top);
-
-    //for(auto p : top) {
-    //    printf("(%lld, %lld): ", get<0>(p.first), get<1>(p.first));
-    //    for(auto x : p.second) {
-    //        printf("%lld, ", x);
-    //    }
-    //    printf("\n");
-    //}
-
     ll count = count_paths(k, n-1, m-1, n, m, a, top);
     printf("%lld\n", count);
 }
